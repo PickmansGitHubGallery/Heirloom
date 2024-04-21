db217afea261eb44c89264191d98a501f8587155
Jakob F <Jakobfoersom88@gmail.com>
2024-04-21 08:18:51 +0200

    små rettelser

--- UI/visAllePersoner.php
+++ UI/visAllePersoner.php
@@ -21,6 +21,7 @@ $allePersoner = visAllePersoner($navn);
 <table>
     <tr>
         <th>ID</th>
+        <th>Vis Profil</th>
         <th>Fornavn</th>
         <th>Efternavn</th>
         <th>Fødselsdag</th>
@@ -35,11 +36,13 @@ $allePersoner = visAllePersoner($navn);
         <th>Slet</th>
     </tr>
     <?php foreach ($allePersoner as $person) : ?>
+        <?php $personlink = "visPersonProfil.php?id=" . $person['id']; ?>
         <?php $morlink = "visPersonProfil.php?id=" . $person['morId']; ?>
         <?php $farlink = "visPersonProfil.php?id=" . $person['farId']; ?>
         <tr>
             <?php $personId = $person['id']; ?>
             <td><?php echo $person['id']; ?></td>
+            <td><?php echo "<a href=$personlink>" . $person['id'] . "</a>"; ?></td>
             <td><?php echo $person['fornavn']; ?></td>
             <td><?php echo $person['efternavn']; ?></td>
             <td><?php echo $person['fDag']; ?></td>
